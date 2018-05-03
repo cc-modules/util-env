1d0112547e92c59ec7e4c86bf02cddc3649a388c
wangweihua <aztack@163.com>
2018-05-03 12:34:27 +0800

    add debug, log fn

--- util-env.js
+++ util-env.js
@@ -1,7 +1,16 @@
 const hostname = location.hostname;
+const reProdHost =/lingobus.com$/;
+
 export default {
   a: !!hostname.match(/^(a[0-9]+)-/),
   local: !!hostname.match(/^(test-|localhost)/),
-  prod: /lingobus.com$/.test(hostname),
-  lang: (window.navigator.language || window.browserLanguage)
+  prod: reProdHost.test(hostname),
+  lang: (window.navigator.language || window.browserLanguage),
+  debug () {
+    if (this.prod) return;
+    console.log.apply(console, arguments);
+  },
+  log () {
+    console.log.apply(console, arguments);
+  }
 };
